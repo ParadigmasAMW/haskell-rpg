f496a1969822a949484e180df1efa27561b23398
Álvaro Fernando <alvarofernandoms@gmail.com>
2015-04-26 18:28:13 -0300

    Init BinaryTree.hs
    Signed-off-by: Álvaro Fernando <alvarofernandoms@hotmail.com>
    Signed-off-by: Matheus Fernandes <matheus.souza.fernandes@gmail.com>
    Signed-off-by: Wilker Mesquita <wilker-mc@hotmail.com>

---
+++ BinaryTree.hs
@@ -0,0 +1,38 @@
+module BinaryTree (history, buildTree, printTree, preOrder) where
+
+import System.Process
+
+data ArvBin elemento = Nulo
+	|Node elemento (ArvBin elemento)(ArvBin elemento)
+	deriving (Show, Eq)
+
+clear = system "cls"	
+
+history :: (Eq elemento, Show elemento) => BinaryTree.ArvBin elemento -> IO()
+history (Node elemento Nulo Nulo) = do
+	clear
+	print elemento
+	putStrLn "Fim de jogo"
+history (Node elemento esq dir) = do
+	clear
+	print elemento
+	putStrLn "Faca sua escolha: "
+	path <- readLn
+	if ((path == 1) && (esq /= Nulo)) then 
+			(history (esq)) 
+		else (if ((path == 2) && (dir /= Nulo)) then 
+			(history (dir))
+		else history(Node elemento esq dir))
+
+buildTree :: [elemento] -> BinaryTree.ArvBin elemento
+buildTree [] = Nulo
+buildTree list = (Node (list !! half)(buildTree $ take half list) (buildTree $ drop (half+1) list))
+    where half = length list `quot` 2
+
+printTree :: Show elemento => BinaryTree.ArvBin elemento -> [Char]
+printTree (Nulo) = "Empty root."
+printTree (Node elemento esq dir) = unlines (printTreeHelper (Node elemento esq dir))
+
+preOrder :: (Ord elemento) => BinaryTree.ArvBin elemento -> [elemento]
+preOrder Nulo = []
+preOrder (Node elemento esq dir) = [elemento] ++ (preOrder esq) ++ (preOrder dir)
